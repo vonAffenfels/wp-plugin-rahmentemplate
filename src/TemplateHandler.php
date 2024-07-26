20fb6ee14fa506e1762cc8513bae3830a4b2e0b7
schmechel <charalambos.schmechel@vonaffenfels.de>
2024-07-26 13:02:32 +0200

    replace url paths for tags

--- src/TemplateHandler.php
+++ src/TemplateHandler.php
@@ -21,13 +21,38 @@ class TemplateHandler
         if (empty($templateUrl) || !filter_var($templateUrl, FILTER_VALIDATE_URL)) {
             echo 'Invalid template URL.';
             exit;
-        }
+        } 
 
         try {
             $template = $client->request('GET', $templateUrl);
             $templateBody = $template->getBody()->getContents();
 
-            $contentReplacedTemplate = str_replace('CONTENT', $content, $templateBody);
+            $dom = new \DOMDocument();
+            @$dom->loadHTML($templateBody);
+
+            $parsedUrl = parse_url($templateUrl);
+            $baseUrl = $parsedUrl['scheme'] . '://' . $parsedUrl['host'];
+            $baseUrl .= rtrim(dirname($parsedUrl['path']), '/') . '/';
+
+            $tags = [
+                'img' => 'src',
+                'link' => 'href',
+                'script' => 'src'
+            ];
+
+            foreach ($tags as $tag => $attribute) {
+                $elements = $dom->getElementsByTagName($tag);
+                foreach ($elements as $element) {
+                    $url = $element->getAttribute($attribute);
+                    if ($url && !parse_url($url, PHP_URL_SCHEME)) {
+                        $element->setAttribute($attribute, $baseUrl . ltrim($url, '/'));
+                    }
+                }
+            }
+
+            $updatedTemplate = $dom->saveHTML();
+
+            $contentReplacedTemplate = str_replace('CONTENT', $content, $updatedTemplate);
 
             echo $contentReplacedTemplate;
         } catch (\Exception $e) {
